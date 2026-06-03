b6de18419b277f2d88134c9bf0654b850ce01ac3
Jyoti Kumari <kumarijyoti0355@gmail.com>
2026-06-03 22:01:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let para = document.querySelector("p");
+
+
+
+let gameSeq = [];
+let useSeq = [];
+
+let start = false;
+let levevl = 0 ;
+
+document.addEventListener("keypress", function(){
+    if (start == false){
+        console.log(`game is started`);
+        start = true;
+    }
+
+
+
+});
